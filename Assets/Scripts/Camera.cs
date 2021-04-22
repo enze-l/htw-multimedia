36eee51245c5333cf15dc55831c004b633ffe159
zink-l <leon.enzenberger@protonmail.com>
2021-04-22 17:11:32 +0200

    Flesh out camera to change between isometric and 3rd Person view

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -6,6 +6,8 @@ public class Camera : MonoBehaviour
 {
     public Transform target;
     Vector3 offset;
+    bool isometricCamera = false;
+    Quaternion rotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,13 +17,18 @@ public class Camera : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        float desiredAngel = target.transform.eulerAngles.y;
-        Quaternion rotation = Quaternion.Euler(0, desiredAngel, 0);
+        if (Input.GetKeyUp(KeyCode.F)) isometricCamera = !isometricCamera;
+        if (!isometricCamera)
+        {
+            float desiredAngel = target.transform.eulerAngles.y;
+            rotation = Quaternion.Euler(0, desiredAngel, 0);
+        }
         transform.position = target.transform.position - (rotation * offset);
 
         var focalPoint = target.transform.position;
-        focalPoint.y +=.6f;
+        focalPoint.y += .6f;
 
         transform.LookAt(focalPoint);
+
     }
 }
