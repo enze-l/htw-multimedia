dc55b8cbc58b2ce88c6b101aed0433a3fcbed894
zink-l <leon.enzenberger@protonmail.com>
2021-05-05 13:04:31 +0200

    Add missing changes to last commit

--- Assets/Scripts/ARController.cs
+++ Assets/Scripts/ARController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ARController : MonoBehaviour
+public class ARController : MonoBehaviour, IVirtualButtonEventHandler
 {
     // Start is called before the first frame update
     void Start()
