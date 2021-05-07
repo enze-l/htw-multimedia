58087b16a01c02e919b005211969c5d5c72c81d6
zink-l <leon.enzenberger@protonmail.com>
2021-05-07 10:37:11 +0200

    Make items spawn if ImageTarget gets visible

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -6,24 +6,36 @@ public class NewBehaviourScript : MonoBehaviour
 {
     public Transform myPrefab;
     public Transform quad;
+    public Renderer quadRenderer;
+    bool shouldBeRendered = false;
     public int objectAmount;
     // Start is called before the first frame update
     void Start()
     {
-        float quadScaleX = quad.transform.localScale.x;
-        float quadScaleY = quad.transform.localScale.y;
-        for(int gem = 0; gem<objectAmount; gem++)
-        {
-            float randomY = Random.Range(-(quadScaleY / 2), quadScaleY/2);
-            float randomX = Random.Range(-(quadScaleX / 2), quadScaleX/2);
-            Instantiate(myPrefab, new Vector3(randomX, 0.2f, randomY), myPrefab.rotation);
-        }
+        
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool beforeState = shouldBeRendered;
+        if(quadRenderer.isVisible){
+            shouldBeRendered = true;
+        }
+        if (beforeState != shouldBeRendered){
+            SpawnObject();
+        }
+    }
 
+    void SpawnObject(){
+        float quadScaleX = quad.transform.localScale.x;
+        float quadScaleY = quad.transform.localScale.y;
+        for(int gem = 0; gem<objectAmount; gem++)
+        {
+            float randomY = Random.Range(-(quadScaleY / 2), quadScaleY/2);
+            float randomX = Random.Range(-(quadScaleX / 2), quadScaleX/2);
+            Instantiate(myPrefab, quad.transform.position + new Vector3(randomX, 0.2f, randomY), myPrefab.rotation, this.transform);
+        }
     }
 }
