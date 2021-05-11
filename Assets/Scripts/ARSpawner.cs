9ace022e1f5700a2ec3c68ff9e1f3d8326cb6093
zink-l <leon.enzenberger@protonmail.com>
2021-05-11 16:50:06 +0200

    Make items spawn the right way around

--- Assets/Scripts/ARSpawner.cs
+++ Assets/Scripts/ARSpawner.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ARSpawner : MonoBehaviour
 {
     public Transform myPrefab;
     public Transform quad;
     public int objectAmount;
+    public Renderer quadRenderer;
+    private List<Transform> items = new List<Transform>();
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +18,20 @@ public class NewBehaviourScript : MonoBehaviour
         {
             float randomY = Random.Range(-(quadScaleY / 2), quadScaleY/2);
             float randomX = Random.Range(-(quadScaleX / 2), quadScaleX/2);
-            Instantiate(myPrefab, new Vector3(randomX, 0.2f, randomY), myPrefab.rotation);
+            Transform blub = Instantiate(myPrefab, this.transform, false);
+            blub.position = new Vector3(randomX, 0.2f, randomY);
+            blub.GetComponent<Renderer>().enabled = false;
+            items.Add(blub);
         }
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(quadRenderer.isVisible){
+            foreach (var item in items){
+                item.GetComponent<Renderer>().enabled = true;
+            }
+        }
     }
 }
